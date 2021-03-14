b35bafcd1de9ce197e778ba2cdd5857a4e81da27
tody <gtody.rabekoto@gmail.com>
2021-03-14 01:32:21 +0100

    phpunit: ready

--- src/Entity/User/Practitioner.php
+++ src/Entity/User/Practitioner.php
@@ -15,6 +15,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\Repository\PractitionerRepository;
 
 /**
  * Class Practitioner
@@ -28,9 +29,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
  *               "groups"={"availability:read"}
  *           }
  *       }
- *   }
+ *      }
  * )
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass=PractitionerRepository::class)
  */
 class Practitioner extends User implements UserInterface
 {
